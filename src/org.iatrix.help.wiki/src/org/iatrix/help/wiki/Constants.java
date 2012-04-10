c5d99099c2062c7f45f1160e069ae323c7e33855
Niklaus Giger <niklaus.giger@member.fsf.org>
2012-04-10 22:40:37 +0200

    Add org.iatrix.help.wiki and de.fhdo.elexis.perspective

---
+++ src/org.iatrix.help.wiki/src/org/iatrix/help/wiki/Constants.java
@@ -0,0 +1,12 @@
+package org.iatrix.help.wiki;
+
+public interface Constants {
+	public static final String CFG_BASE = "org.iatrix.help.wiki";
+	
+	public static final String CFG_BASE_URL = CFG_BASE + "/base_url";
+	public static final String CFG_START_PAGE = CFG_BASE + "/start_page";
+	
+	public static final String DEFAULT_BASE_URL =
+		"http://www.iatrix.org/pmwiki/pmwiki.php/Elexishilfe/";
+	public static final String DEFAULT_START_PAGE = "Uebersicht";
+}
