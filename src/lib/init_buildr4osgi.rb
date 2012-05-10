dd9ad57d1faf82c1c1624147cb44c4fb5642fd4d
Niklaus Giger <niklaus.giger@member.fsf.org>
2012-05-10 16:50:26 +0200

    Fixes for intial build. Correctly recognize missing gems

--- src/lib/init_buildr4osgi.rb
+++ src/lib/init_buildr4osgi.rb
@@ -12,7 +12,7 @@
 
 $onWindows = false
 $onWindows = true if /mingw/.match(RUBY_PLATFORM)
-if /java/i.match(RUBY_PLATFORM) 
+if /java/i.match(RUBY_PLATFORM)
 	require 'java'
 	if /windows/i.match(java.lang.System.getProperty 'os.name')
 		puts "jruby on Windows"
@@ -47,63 +47,58 @@ end
 
 $usePrefix = 'jruby -S'
 if $onWindows
-	if !system('jruby --version', MayFail)
-	  puts "You must install jruby first!"
-	  exit 3
-    else
-	  puts "jruby already installed"
-	end
+  if !system('jruby --version', MayFail)
+    puts "You must install jruby first!"
+    exit 3
+  else
+    puts "jruby already installed"
+  end
 else
-	if File.directory?("#{ENV['HOME']}/.rvm")
-	  puts "rvm already installed"
-	else
-	 system("curl  https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer")
-	 system("bash -s stable < rvm-installer")
-	end
-	$usePrefix = 'rvm jruby do '
-	oldPath = ENV['PATH']
-	ENV['PATH']="#{ENV['HOME']}/.rvm/bin:#{oldPath}"
-	puts  "path is now: "+ENV['PATH']
-	system('rvm --version', MayFail)
-	system('type rvm | /usr/bin/head -1', MayFail)
-	jrubyDir = "#{ENV['HOME']}/.rvm/rubies/jruby*"
-	if !system("#{$usePrefix}jruby --version", MayFail) or Dir.glob(jrubyDir).size == 0
-	  system('rvm install jruby')
-	else
-	  puts "jruby already installed"
-	end
+  if File.directory?("#{ENV['HOME']}/.rvm")
+    puts "rvm already installed"
+  else
+    res = system('curl -L get.rvm.io | bash -s stable')
+    exit 2 if !res
+  end
+  $usePrefix = 'rvm jruby do '
+  oldPath = ENV['PATH']
+  ENV['PATH']="#{ENV['HOME']}/.rvm/bin:#{oldPath}"
+  puts  "path is now: "+ENV['PATH']
+  system('rvm --version', MayFail)
+  system('type rvm | /usr/bin/head -1', MayFail)
+  jrubyDir = "#{ENV['HOME']}/.rvm/rubies/jruby*"
+  if !system("#{$usePrefix}jruby --version", MayFail) or Dir.glob(jrubyDir).size == 0
+    system('rvm install jruby')
+  else
+    puts "jruby already installed"
+  end
 end
 
 needsRebuild = false
 
 def checkGem(gemName, version=nil)
   puts "checkGem #{gemName} #{version}"
-#  rvm jruby do gem list --local buildr | tee tmp.tmp && grep buildr tmp.tmp
-  cmd = "#{$usePrefix} gem list --local #{gemName}  | tee tmp.tmp"
-  cmd += " && grep #{version} tmp.tmp" if version
-  if !system(cmd, MayFail)
-    puts "checkGem: Gem #{gemName} not found. #{version != nil ? 'Should match version '+version : ''}"
-    needsRebuild = true
-    return false
-  else
-    puts "checkGem: Gem #{gemName} already installed"
-    return true
-  end
+  cmd = "#{$usePrefix} gem list --local #{gemName}"
+  output = `#{cmd}`
+  puts output
+  return true if output.index(gemName) and !version
+  return true if output.index("#{gemName} (#{version}")
+  puts "checkGem: Gem #{gemName} not found. #{version != nil ? 'Should match version '+version : ''}"
   return false
 end
 
-exit 3 if !checkGem('buildrizpack', '0.2')
-exit 3 if !checkGem('buildr', '1.4.6')
-['net-ldap', 'buildrdeb'].each do 
-  |name|
-    if !checkGem(name)
+{'net-ldap' => nil, 'buildrdeb' => nil,'buildrizpack' =>'0.2','buildr' => '1.4.'}.each do
+  |name, version|
+    if !checkGem(name,version)
       system("#{$usePrefix} gem install #{name}")
+    else
+      puts "gem #{name} is already installed"
     end
 end
 
 buildr4osgiPath = "#{ENV['HOME']}/buildr4osgi"
 buildr4osgiInstalled = checkGem('buildr4osgi', '0.9.6.93')
-            
+
 if needsRebuild || Dir.glob(buildr4osgiPath).size == 0 || !buildr4osgiInstalled
   puts "Adding buildr4osgi (special from niklaus)"
   system("git clone git://github.com/ngiger/buildr4osgi.git #{buildr4osgiPath}") if !File.directory?(buildr4osgiPath)
